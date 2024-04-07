cca853ead6a91cdb9c0d000fcaec984ceaff14ff
Izanalgit <izanalvarez@gmail.com>
2024-04-07 21:55:27 +0200

    fix: background images routes

--- script/background.js
+++ script/background.js
@@ -1,16 +1,16 @@
 const bgLib = [
-    "./../assets/imgs/m1.png",
-    "./../assets/imgs/m2.png",
-    "./../assets/imgs/m3.png",
-    "./../assets/imgs/m4.png",
-    "./../assets/imgs/m5.png",
-    "./../assets/imgs/m6.png",
-    "./../assets/imgs/m7.png",
-    "./../assets/imgs/m8.png",
-    "./../assets/imgs/m9.png",
-    "./../assets/imgs/m10.png",
-    "./../assets/imgs/m11.png",
-    "./../assets/imgs/m12.png"
+    "./assets/imgs/m1.png",
+    "./assets/imgs/m2.png",
+    "./assets/imgs/m3.png",
+    "./assets/imgs/m4.png",
+    "./assets/imgs/m5.png",
+    "./assets/imgs/m6.png",
+    "./assets/imgs/m7.png",
+    "./assets/imgs/m8.png",
+    "./assets/imgs/m9.png",
+    "./assets/imgs/m10.png",
+    "./assets/imgs/m11.png",
+    "./assets/imgs/m12.png"
 ];
 
 // const randomBackground = (lib) => Math.trunc((Math.random() * lib.length));
@@ -19,9 +19,9 @@ let counter = 0;
 setInterval(() => {
     counter++;
     if (counter >= bgLib.length) counter = 0;
-    document.body.style.backgroundImage = `url("${bgLib[counter]}")`;
+    document.body.style.background = `url("${bgLib[counter]}")`;
     
-    // document.body.style.backgroundImage = `url("${bgLib[randomBackground(bgLib)]}")`;
+    // document.body.style.background = `url("${bgLib[randomBackground(bgLib)]}")`;
 },2000);
 
 //en random no se aprecia caer el codigo como en las pelis, por eso le he puesto un counter y en orden
